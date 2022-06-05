d8cad3a7fb933ba7dd35d8586661c41fa9e59ad0
akara19 <101150250+akara19@users.noreply.github.com>
2022-06-06 06:26:04 +0700

    Add files via upload

--- Bahan/Template/menu.php
+++ Bahan/Template/menu.php
@@ -1,27 +1,45 @@
-	<nav>
-    <div class="nav-item">
-      <a class="nav-link active" href="index.php">Home</a>
-    </div>
-    <div class="nav-item">
-      <a class="nav-link" href="">Array</a>
-      <div class="nav-dropdown">
-        <a href="array_barang.php">Data Barang (Statis)</a>
-        <a href="array_barang_dinamis.php">Data Barang (Dinamis)</a>
-      </div>
-    </div>
-		
-   <div class="nav-item">
-      <a class="nav-link" href="kontak.html">Pointer dan Linked List</a>
+ <nav>
+	<div class="nav-item">
+		<a class="nav-link active" href="index.php">Home</a>
+	</div>
+	<div class="nav-item">
+		<a class="nav-link" href="">Array</a>
+		<div class="nav-dropdown">
+			<a href="array_barang.php">Data Barang (Statis)</a>
+			<a href="array_barang_dinamis.php?clear=1">Data Barang (Dinamis)</a>
+
+		</div>
+	</div>
+	<div class="nav-item">
+		<a class="nav-link" href="kontak.html">Pointer dan Linked List</a>
 		<div class="nav-dropdown">
 			<a href="pointer.php">Pointer</a>
 			<a href="linked_list.php">Linked List</a>
 		</div>
-    </div>
-   
-   <div class="nav-item">
-      <a href="" class="nav-link">Tree</a>
-      <div class="nav-dropdown">
-        <a href="tree.php">Tree</a>
-      </div>
-    </div>
-  </nav>
+	</div>
+
+	<div class="nav-item">
+		<a href="" class="nav-link">Tree</a>
+		<div class="nav-dropdown">
+			<a href="tree.php">Tree</a>
+		</div>
+	</div>
+
+	<div class="nav-item">
+		<a href="" class="nav-link">Pemrograman File</a>
+		<div class="nav-dropdown">
+			<a href="menulis_file.php">Create File</a>
+			<a href="menulis_file02.php">Create File 2</a>
+			<a href="Json.php">Create File Dengan Json</a>
+		</div>
+	</div>
+	
+
+	<div class="nav-item">
+		<a href="" class="nav-link">Pemrograman Graph</a>
+		<div class="nav-dropdown">
+			<a href="Grafik.php">Grafik 01</a>
+		</div>
+	</div>
+
+</nav>
